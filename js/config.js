7c91ee49f20affce2d6183cec138d3078e2672b8
littleblackman <sandyrazafitrimo@gmail.com>
2019-01-15 11:51:41 +0100

    no commit on config

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
-var apiKey  = "4a7c07f50800222e53c80f2dc5f61d36";
+var apiKey = "4a7c07f50800222e53c80f2dc5f61d36";
 var moviedb = "https://api.themoviedb.org/3/movie/";
+// init globals vars
 var intervalID;
